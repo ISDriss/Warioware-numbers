e9c12dd1b1b01b05832909ef6670ccfc55169b13
Ilian SEBTI <ilian.sebti@gmail.com>
2025-02-07 15:33:46 +0100

    'undefined' bug fix

--- DDN standalone/script.js
+++ DDN standalone/script.js
@@ -85,8 +85,8 @@ async function predictAndDraw() {
 }
 
 function writeNumber(number) {
-    if (number !== 'Neutral') {
-        number = number[6]; //the class names are like "Class 1", "Class 2", etc. or Neutral
+    if (number !== 'Neutre' || number !== 'undefined') {
+        number = number[6]; //the class names are like "Class 1", "Class 2", etc. or Neutre
         const numberElement = document.createElement('span');
         numberElement.innerText = number + ' ';
         document.getElementById('number-container').appendChild(numberElement);
